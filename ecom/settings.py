b0b78f373f354eac46b833f91203bcf2d1e01dd0
nshaibu <nafiushaibu1@gmail.com>
2020-07-07 22:57:51 +0000

    delete files

--- ecom/settings.py
+++ ecom/settings.py
@@ -137,10 +137,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # DUMMY EMAIL BACKEND
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
-
-# EMAIL_HOST = 'smtp.sendgrid.net'
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'testsite_app'
-# EMAIL_HOST_PASSWORD = 'mys3cr3tp4ssw0rd'
-# EMAIL_USE_TLS = True
-# DEFAULT_FROM_EMAIL = 'TestSite Team <noreply@example.com>'
